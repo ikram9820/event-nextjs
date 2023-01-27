a4963f24b363c1d24d71aeff64426f978c01aa3e
unknown <ikram9820khan@gmail.com>
2023-01-27 12:37:32 +0500

    mongodb is added to newsletter

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,12 +1,20 @@
 import * as utils from "@/utils";
+import { MongoClient } from "mongodb";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const email = req.body.email;
-    const path = utils.getDataFilePath("emails");
-    let emails = utils.getAllEmails(path);
-    emails.push({email});
-    utils.addNewsletterEmail(path, emails);
+    // const path = utils.getDataFilePath("emails");
+    // let emails = utils.getAllEmails(path);
+    // emails.push({email});
+    // utils.addNewsletterEmail(path, emails);
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://ikram:8KCXyzoXoNtnjmAe@nextjs-course.mopw532.mongodb.net/?retryWrites=true&w=majority"
+    );
+    const db = client.db("nextjs-course");
+    await db.collection("emails").insertOne({ email });
+    client.close();
     res.status(201).json({ message: "subscribed successfully " });
   } else {
     res.status(405);
